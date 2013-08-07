912346ab13e36b54c685741051711066de3ddc7d
pc035860 <pc035860@gmail.com>
2013-08-07 16:36:02 +0800

    extract "plunkSource" module

--- plunk-source/app.js
+++ plunk-source/app.js
@@ -1,4 +1,4 @@
-angular.module('plunkSource', ['ngSelect', 'hljs'])
+angular.module('plunkSourceApp', ['plunkSource'])
 
 .constant('psConst', {
   THEME_MAP: {
@@ -45,54 +45,6 @@ angular.module('plunkSource', ['ngSelect', 'hljs'])
   .otherwise({
     redirectTo: '/OPxzDu'
   });
-})
-
-.factory('plunkData', function($http) {
-  return function (plunkId) {
-    var api = "http://api.plnkr.co/plunks/";
-    return $http.get(api + plunkId)
-      .then(function (res) {
-        return res.data;
-      });
-  };
-})
-
-.directive('plunkSource', function (plunkData) {
-  return {
-    restrict: 'EA',
-    scope: {
-      plunkId: "@plunkSource"
-    },
-    templateUrl: 'plunk-source-template.html',
-    link: function(scope, iElm, iAttrs) {
-      scope.fileIndex = null;
-      scope.currentFilename = null;
-      scope.files = [];
-
-      scope.$watch('plunkId', function (newId, oldId) {
-        if (angular.isString(newId) && newId) {
-          scope.fileIndex = {};
-          scope.currentFilename = null;
-          scope.files.length = 0;
-
-          plunkData(newId)
-          .then(function (data) {
-            angular.forEach(data.files, function (fileInfo, name) {
-              var obj = {
-                name: name,
-                content: fileInfo.content
-              };
-              scope.files.push(obj);
-              scope.fileIndex[name] = obj;
-            });
-
-            scope.currentFilename = scope.files[0].name;
-          });
-        }
-
-      });
-    }
-  };
 })
 
 .controller('PlunkCtrl', function($scope, $routeParams, psConst) {
