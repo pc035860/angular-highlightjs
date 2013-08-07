06c337d2d8412f7e6fdfb5638e6ce927baa19996
pc035860 <pc035860@gmail.com>
2013-08-07 16:36:02 +0800

    extract "plunkSource" module

---
+++ plunk-source/plunkSource.js
@@ -0,0 +1,49 @@
+angular.module('plunkSource', ['ngSelect', 'hljs'])
+
+.factory('_apiPlunkData', function($http) {
+  return function (plunkId) {
+    var api = "http://api.plnkr.co/plunks/";
+    return $http.get(api + plunkId)
+      .then(function (res) {
+        return res.data;
+      });
+  };
+})
+
+.directive('plunkSource', function (_apiPlunkData) {
+  return {
+    restrict: 'EA',
+    scope: {
+      plunkId: "@plunkSource"
+    },
+    templateUrl: 'plunk-source-template.html',
+    link: function(scope, iElm, iAttrs) {
+      scope.fileIndex = null;
+      scope.currentFilename = null;
+      scope.files = [];
+
+      scope.$watch('plunkId', function (newId, oldId) {
+        if (angular.isString(newId) && newId) {
+          scope.fileIndex = {};
+          scope.currentFilename = null;
+          scope.files.length = 0;
+
+          _apiPlunkData(newId)
+          .then(function (data) {
+            angular.forEach(data.files, function (fileInfo, name) {
+              var obj = {
+                name: name,
+                content: fileInfo.content
+              };
+              scope.files.push(obj);
+              scope.fileIndex[name] = obj;
+            });
+
+            scope.currentFilename = scope.files[0].name;
+          });
+        }
+
+      });
+    }
+  };
+})
