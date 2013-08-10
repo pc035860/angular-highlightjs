4a3d20e1978ba4e0d7a1bad36b436e1e017d413d
pc035860 <pc035860@gmail.com>
2013-08-10 18:21:37 +0800

    restructure path & options v2.0.0

--- plunk-source/scripts/app.js
+++ plunk-source/scripts/app.js
@@ -39,45 +39,10 @@ angular.module('plunkSourceApp', ['plunkSource'])
 .config(function ($routeProvider) {
   $routeProvider
   .when('/plunk/:plunkId*options', {
-    templateUrl: 'main.html',
-    controller: 'PlunkCtrl'
+    templateUrl: 'partials/main.html',
+    controller: 'MainCtrl'
   })
   .otherwise({
     redirectTo: '/plunk/OPxzDu'
   });
-})
-
-.controller('PlunkCtrl', function($scope, $routeParams, psConst) {
-
-  $scope.plunkId = $routeParams.plunkId;
-  $scope.options = _parseOptions($routeParams.options);
-
-  $scope.containerTheme = 'light';
-  $scope.highlightTheme = 'default';
-  if ($scope.options.theme in psConst.THEME_MAP) {
-    $scope.highlightTheme = $scope.options.theme;
-
-    $scope.containerTheme = psConst.THEME_MAP[$scope.options.theme];
-  }
-
-  function _parseOptions(locationHash) {
-    var c = 0,
-        keyBuf = null,
-        output = {};
-    angular.forEach(locationHash.split(/\//), function (piece) {
-      if (piece != '') {
-        if (c % 2 === 0) {
-          // key
-          keyBuf = piece;
-        }
-        else if (keyBuf !== null) {
-          // value
-          output[keyBuf] = piece;
-          keyBuf = null;
-        }
-        c++;
-      }
-    });
-    return output;
-  }
 });
