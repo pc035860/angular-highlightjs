e60f77eb7b3a4736e80ccf356aac22c977769d22
pc035860 <pc035860@gmail.com>
2015-02-03 18:27:19 +0800

    Update example to v0.4.1

--- with-browserify/scripts/bundle.js
+++ with-browserify/scripts/bundle.js
@@ -1,8 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*! angular-highlightjs
-version: 0.4.0
+version: 0.4.1
 build date: 2015-02-03
-author: Robin Fan
+author: Chih-Hsuan Fan
 https://github.com/pc035860/angular-highlightjs.git */
 
 /* commonjs package manager support (eg componentjs) */
